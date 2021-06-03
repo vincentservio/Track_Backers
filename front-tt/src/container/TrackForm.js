1ad57408980ea79cc71199337a5076a3575cc32d
vincentservio <vincentservio@gmail.com>
2021-06-02 22:13:12 -0400

    fieldset

--- front-tt/src/container/TrackForm.js
+++ front-tt/src/container/TrackForm.js
@@ -48,46 +48,49 @@ class TrackForm extends Component {
     return (
       <div>
         <form onSubmit={this.handleSubmit}>
-          <h2>Add New Track</h2>
-          Title:
-          <input
-            type="text"
-            name="title"
-            value={this.state.title}
-            onChange={this.handleChange}
-          />
-          Instrumental:
-          <input
-            type="text"
-            name="instrumental"
-            value={this.state.instrumental}
-            onChange={this.handleChange}
-          />
-          Notes:
-          <textarea
-            name="notes"
-            value={this.state.notes}
-            onChange={this.handleChange}
-          />
-          <br />
-          Status:
-          <select
-            name="status"
-            value={this.state.status}
-            onChange={this.handleChange}
-          >
-            <option value="Unknown">Choose Status / Default "Unknown"</option>
-            <option value="Complete">Complete</option>
-            <option value="Written But Not Recorded">
-              Written, But Not Recorded
-            </option>
-            <option value="Partialy Written And Recorded">
-              Partialy Written & Recorded
-            </option>
-            <option value="Lyrics No Beat">Lyrics No Instrumental</option>
-            <option value="Beat No Lyrics">Instrumental No Lyrics </option>
-          </select>
-          <input type="submit" value="Submit Track" />
+          <fieldset>
+            <legend>Add New Track</legend>
+            {/* <h2>Add New Track</h2> */}
+            Title:
+            <input
+              type="text"
+              name="title"
+              value={this.state.title}
+              onChange={this.handleChange}
+            />
+            Instrumental:
+            <input
+              type="text"
+              name="instrumental"
+              value={this.state.instrumental}
+              onChange={this.handleChange}
+            />
+            Notes:
+            <textarea
+              name="notes"
+              value={this.state.notes}
+              onChange={this.handleChange}
+            />
+            <br />
+            Status:
+            <select
+              name="status"
+              value={this.state.status}
+              onChange={this.handleChange}
+            >
+              <option value="Unknown">Choose Status / Default "Unknown"</option>
+              <option value="Complete">Complete</option>
+              <option value="Written But Not Recorded">
+                Written, But Not Recorded
+              </option>
+              <option value="Partialy Written And Recorded">
+                Partialy Written & Recorded
+              </option>
+              <option value="Lyrics No Beat">Lyrics No Instrumental</option>
+              <option value="Beat No Lyrics">Instrumental No Lyrics </option>
+            </select>
+            <input type="submit" value="Submit Track" />
+          </fieldset>
         </form>
         <img
           src="https://i.pinimg.com/originals/ab/45/bb/ab45bb4451536652faca51ae4f42d5dd.gif"
